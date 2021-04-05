ad2dd979d2acda4aa2ccb07961c9dbfd1a6a2da9
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-04-06 00:19:26 +0530

    created custom hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useForm } from "./useForm";
 
-function App() {
+const App = () => {
+  const [values, handleChange] = useForm({ email: "", password: "" });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="email" value={values.email} onChange={handleChange} />
+      <input type="password" name="password" value={values.password} onChange={handleChange} />
     </div>
   );
-}
+};
 
 export default App;
